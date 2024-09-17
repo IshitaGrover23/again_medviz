2ef52c18241edbb72dbd25d245c9015a4ee74a4a
Jasleen200000001 <jasskaurrupal@gmail.com>
2024-09-17 15:13:25 +0530

    setting

--- frontend/settings.py
+++ frontend/settings.py
@@ -107,15 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'sumapp', 'static'),
-    os.path.join(BASE_DIR, 'frontend', 'recommender', 'static'),
-    os.path.join(BASE_DIR, 'frontend', 'diabetes', 'static'),
-    os.path.join(BASE_DIR, 'frontend', 'website', 'static'),
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'sumapp', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
